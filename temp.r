789995598a58b3fdb5a19f31cb5995b827716df4
xiatian52673 <xiatian52673@gmail.com>
2018-11-25 15:48:18 -0500

    path

--- temp.r
+++ temp.r
@@ -1,3 +1,3 @@
-x <- read.csv("~/Downloads/temp/tae.data", header = FALSE)
-x[2,2]
-
+heart_statlog = read.table("http://archive.ics.uci.edu/ml/machine-learning-databases/statlog/heart/heart.dat")
+breast_cancer = read.csv("https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer/breast-cancer.data")
+breast_w = read.csv("https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data")
