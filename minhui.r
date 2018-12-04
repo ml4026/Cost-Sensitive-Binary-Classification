914f03a224c267b0b2a871c70cd7e28ed1d5420c
Jimenius <l.minhui@qq.com>
2018-12-03 19:55:47 -0500

    Comments

--- minhui.r
+++ minhui.r
@@ -19,20 +19,24 @@ for (ck in 1:2){
 }
 
 u = 0 #uncertainty
-data.real <- data #Real data
+#data.real <- data #Real data
+set.seed(6690)
 cost.test <- runif(9, min = 0, max = 100) #Test cost of each attribute
-cost.FP <- 600
+cost.FP <- 1000
 cost.FN <- 1000
-A.known <- c()
-A.unknown <- 2:10
-cost.exp.test <- 0
+
+cost.exp <- rep(0, nrow(data)) #Cost of each sample
 
 for (j in 1:nrow(data)){
+  # Initialization for the sample
+  cost.exp.test <- 0
+  A.known <- c()
+  A.unknown <- 2:10
   #Pij
   P.ij <- array(0, dim=c(9,10))
   for (attrib in 1:9){
     for (v in 1:10){
-      P.ij[attrib, v] <- u / 8
+      P.ij[attrib, v] <- u / 9
     }
     P.ij[attrib, data[j, attrib + 1]] <- 1 - u
   }
@@ -44,20 +48,20 @@ for (j in 1:nrow(data)){
     cost.exp.unknown <- rep(0, l)
     for (i in 1:l){
       
-      P.ACk <- c(1, 1)
-      P.AACk <- c(1, 1)
+      P.ACk <- c(1, 1) #P(A|Ck)
+      P.AACk <- c(1, 1) #P(A,A|Ck)
       for (ck in 1:2){
-        for (attrib in A.known){
+        for (attrib in A.known){ #No iterations if empty
           P.AiCk <- sum(P.ij[attrib,] * P.VmCk[attrib,,ck])
           P.ACk[ck] <- P.ACk[ck] * P.AiCk 
         }
-        P.AuCk <- sum(P.ij[A.unknown[i] - 1,] * P.VmCk[A.unknown[i] - 1,,ck])
+        P.AuCk <- sum(P.ij[A.unknown[i] - 1,] * P.VmCk[A.unknown[i] - 1,,ck]) #Include Attribute A.unknown[i]
         P.AACk[ck] <- P.ACk[ck] * P.AuCk
       }
-      P.CkA <- c(1, 1)
-      P.CkAA <- c(1, 1)
-      P.A <- sum(P.ACk * N.Ck) / nrow(data)
-      P.AA <- sum(P.AACk * N.Ck) / nrow(data)
+      P.CkA <- c(1, 1) #P(Ck|A)
+      P.CkAA <- c(1, 1) #P(Ck|A,A)
+      P.A <- sum(P.ACk * N.Ck) / nrow(data) #P(A)
+      P.AA <- sum(P.AACk * N.Ck) / nrow(data) #P(A,A)
       for (ck in 1:2){
         P.CkA[ck] <- P.ACk[ck] * N.Ck[ck] / nrow(data) / P.A
         P.CkAA[ck] <- P.AACk[ck] * N.Ck[ck] / nrow(data) / P.AA
@@ -85,7 +89,8 @@ for (j in 1:nrow(data)){
     A.known <- c(A.known, max.attr) #Include new test in the known attribute
     A.unknown <- A.unknown[A.unknown != max.attr] #Exclude new test in the unknown attribute
   }
+  cost.exp[j] <- cost.exp.known + cost.exp.test
 }
-
+mean(cost.exp)
  
 
