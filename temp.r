3b6ed11c3a6bb7b2d11f58cf622583a90b2a8a6d
Jimenius <l.minhui@qq.com>
2018-12-18 14:42:04 -0500

    Delete unecessary

--- temp.r
+++
@@ -1,3 +0,0 @@
-heart_statlog = read.table("http://archive.ics.uci.edu/ml/machine-learning-databases/statlog/heart/heart.dat")
-breast_cancer = read.csv("https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer/breast-cancer.data")
-breast_w = read.csv("https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data")
